80e815208e5cbdb21ab65228db3b4e30e5bce10d
Andrio <andrio.rodrigo.silva@hotmail.com>
2023-08-05 14:04:07 -0300

    create and add logger

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"github.com/andrioc/job-opening-api/config"
 	"github.com/andrioc/job-opening-api/router"
 )
 
 func main() {
+	logger := config.GetLogger("main")
+	err := config.Init()
+
+	if err != nil {
+		logger.ErrorF("config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
