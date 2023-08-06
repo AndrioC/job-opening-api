3fdccf7de7efd141552a71c6ecb034a087f6a247
Andrio <andrio.rodrigo.silva@hotmail.com>
2023-08-06 13:00:05 -0300

    add show opening implementation

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -3,11 +3,16 @@ package handler
 import (
 	"net/http"
 
+	"github.com/andrioc/job-opening-api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Openings",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listininge erros")
+	}
+
+	sendSuccess(ctx, "list-openings", openings)
 }
