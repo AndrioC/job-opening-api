1357892643eca1e8bea3808e74a8c757c377e2af
Andrio <andrio.rodrigo.silva@hotmail.com>
2023-08-06 13:24:04 -0300

    add UpdatedAt fielt to UpdateOpeningRequest struct

--- handler/request.go
+++ handler/request.go
@@ -1,5 +1,7 @@
 package handler
 
+import "time"
+
 type CreateOpeningRequest struct {
 	Role     string `json:"role" validate:"required"`
 	Company  string `json:"company" validate:"required"`
@@ -10,10 +12,11 @@ type CreateOpeningRequest struct {
 }
 
 type UpdateOpeningRequest struct {
-	Role     string `json:"role" validate:"omitempty"`
-	Company  string `json:"company" validate:"omitempty"`
-	Location string `json:"location" validate:"omitempty"`
-	Remote   *bool  `json:"remote" validate:"omitempty"`
-	Link     string `json:"link" validate:"omitempty"`
-	Salary   int64  `json:"salary" validate:"omitempty,min=1"`
+	Role      string    `json:"role" validate:"omitempty"`
+	Company   string    `json:"company" validate:"omitempty"`
+	Location  string    `json:"location" validate:"omitempty"`
+	Remote    *bool     `json:"remote" validate:"omitempty"`
+	Link      string    `json:"link" validate:"omitempty"`
+	Salary    int64     `json:"salary" validate:"omitempty,min=1"`
+	UpdatedAt time.Time `json:"-"`
 }
