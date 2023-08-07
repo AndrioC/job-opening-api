7b5b1487dabf367ba7037f79a7b23d1bfc7fe098
Andrio <andrio.rodrigo.silva@hotmail.com>
2023-08-07 17:04:52 -0300

    fix docs for deleteOpening and add DeleteOpening response

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -15,11 +15,11 @@ import (
 // @Tags Openings
 // @Accept json
 // @Produce json
-// @Param request body DeleteOpeningRequest true "Request body"
+// @Param id query string true "Opening identification"
 // @Success 200 {object} DeleteOpeningResponse
 // @Failure 400 {object} ErrorResponse
 // @Failure 404 {object} ErrorResponse
-// @Router /opening [post]
+// @Router /opening [delete]
 func DeleteOpeningHandler(ctx *gin.Context) {
 	id := ctx.Query("id")
 
